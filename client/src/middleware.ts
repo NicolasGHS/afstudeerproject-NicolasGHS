5db6e73c5a8803680fe56c2b024173d7595f88bd
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-03-23 17:28:07 +0100

    add protected routes

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+
+export default clerkMiddleware( async (auth) => {
+    await auth.protect();
+});
+
 
 export const config = {
   matcher: [
